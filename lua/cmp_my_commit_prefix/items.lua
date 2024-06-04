c6fb3881221ff44526e05ad48df76edcee0c010d
staticWagomU <to6wa17@gmail.com>
2024-06-04 09:22:03 +0900

    feat: labelにconversion commitのprefixを併記

--- lua/cmp_my_commit_prefix/items.lua
+++ lua/cmp_my_commit_prefix/items.lua
@@ -1,13 +1,13 @@
 return function()
   return {
-    { word = ':feat:'; label = '新機能'; insertText = 'feat: '; filterText = ':feat:' },
-    { word = ':fix:'; label = 'バグ修正'; insertText = 'fix: '; filterText = ':fix:' },
-    { word = ':docs:'; label = 'ドキュメントのみ'; insertText = 'docs: '; filterText = ':ci:' },
-    { word = ':style:'; label = 'コードの意味に影響がない'; insertText = 'style: '; filterText = ':style:' },
-    { word = ':refactor:'; label = 'バグ修正でも機能追加でもない'; insertText = 'refactor: '; filterText = ':refactor:' },
-    { word = ':perf:'; label = 'パフォーマンスを向上させる'; insertText = 'perf: '; filterText = ':perf:' },
-    { word = ':test:'; label = 'テスト関連'; insertText = 'test: '; filterText = ':test:' },
-    { word = ':ci:'; label = 'ci関連'; insertText = 'ci: '; filterText = ':ci:' },
-    { word = ':chore:'; label = 'その他'; insertText = 'chore: '; filterText = ':chore:' },
+    { word = ':feat:'; label = '(feat)新機能'; insertText = 'feat: '; filterText = ':feat:' },
+    { word = ':fix:'; label = '(fix)バグ修正'; insertText = 'fix: '; filterText = ':fix:' },
+    { word = ':docs:'; label = '(docs)ドキュメントのみ'; insertText = 'docs: '; filterText = ':ci:' },
+    { word = ':style:'; label = '(style)コードの意味に影響がない'; insertText = 'style: '; filterText = ':style:' },
+    { word = ':refactor:'; label = '(refactor)バグ修正でも機能追加でもない'; insertText = 'refactor: '; filterText = ':refactor:' },
+    { word = ':perf:'; label = '(perf)パフォーマンスを向上させる'; insertText = 'perf: '; filterText = ':perf:' },
+    { word = ':test:'; label = '(test)テスト関連'; insertText = 'test: '; filterText = ':test:' },
+    { word = ':ci:'; label = '(ci)ci関連'; insertText = 'ci: '; filterText = ':ci:' },
+    { word = ':chore:'; label = '(chore)その他'; insertText = 'chore: '; filterText = ':chore:' },
   }
 end
